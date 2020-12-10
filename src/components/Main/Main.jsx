54653d645835dd8f3f669f47ce048a513245050b
dmaslov <dmaslov@neoflex.ru>
2020-12-10 13:51:26 +0300

    commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
 export const Main = () => {
+    let addNewMessage = () => {
+        console.log('addNewMessage')
+    }
+
     return (
         <div>
-            Main
+            <AddMessageFormRedux onSubmit={addNewMessage} />
         </div>
     );
 }
+
+const AddMessageForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component="textarea" name="newMessageBody" placeholder="Enter your message" />
+            </div>
+            <div><button>Send</button></div>
+        </form>
+    );
+}
+
+const AddMessageFormRedux = reduxForm({form: "dialogAddMessageForm"})(AddMessageForm);
