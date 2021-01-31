1062b0fd56bd0fa85826db69432c737d8f5027d4
dmaslov <dmaslov@neoflex.ru>
2021-02-01 02:30:57 +0300

    commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {UseStateComponent} from "../hook/useState/useState";
 import {UseEffect} from "../hook/useEffect/useEffect";
+import {UseContext} from "../hook/useContext/useContext";
 
 export const Main = () => {
     return (
         <div>
             {/*<UseStateComponent />*/}
-            <UseEffect />
+            {/*<UseEffect />*/}
+            <UseContext />
         </div>
     );
 }
